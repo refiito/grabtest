2e5e5b5c09964b617abc1d299704e01fc79af904
Margus Mahler <margus@toggl.com>
2016-11-26 14:47:50 +0200

    simple systemd unit to control the testapp

--- site-cookbooks/helloapp/recipes/default.rb
+++ site-cookbooks/helloapp/recipes/default.rb
@@ -9,12 +9,45 @@ end
 bash 'compile-app' do
   environment ({
     'GOROOT' => "#{node['go']['install_dir']}/go",
-    'GOBIN'  => '$GOROOT/bin',
+    'GOBIN'  => '/opt/testapp/bin',
     'GOPATH' => '/opt/go'
   })
   code "cd /opt/testapp && /usr/local/go/bin/go get && /usr/local/go/bin/go build -o testapp"
   action :run
   only_if {not ::File.exist?('/opt/testapp/testapp')}
+end
+
+file "/opt/testapp/testapp.conf" do
+  content <<-CONF
+DB="host=#{node['postgres']['host']} port=#{node['postgres']['port']} dbname=gtest user=thechief password=securesecure sslmode=require"
+CONF
+end
+
+systemd_unit "testapp.service" do
+  enabled true
+  content <<-CONF
+[Unit]
+Description=testapp
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+EnvironmentFile=/opt/testapp/testapp.conf
+WorkingDirectory=/opt/testapp
+ExecStart=-/opt/testapp/testapp
+Restart=on-failure
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+Alias=testapp.service
+CONF
+  action :create
+end
+
+service "testapp" do
+  provider Chef::Provider::Service::Systemd
+  supports status: true, restart: true
+  action :start
 end
 
 file "#{node['nginx']['default_root']}/index.html" do
